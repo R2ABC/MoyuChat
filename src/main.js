4614731052e73cd21934c9f18cb11c5c441a0b11
SurKaa <suralcarywn@qq.com>
2023-08-06 00:42:25 +0800

    引入并使用ElementUI

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 // 引入页面属性重置
 import '@/style/reset.scss'
+// 使用ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 new Vue({
   router,
